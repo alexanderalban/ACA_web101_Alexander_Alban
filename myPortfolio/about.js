a90c8479b62deec35d747a03413cb0c1737fa386
alexanderalban <albanalexander@gmail.com>
2020-09-30 00:12:46 -0500

    popups!

--- myPortfolio/about.js
+++ myPortfolio/about.js
@@ -1,2 +1,25 @@
-const element = document.querySelector(#blog);
-element.classlist.add('animate_animated',animate_bounceInRight)
+function hobbypop1() {
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+  function hobbypop2() {
+    const popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+
+  function hobbypop3() {
+      const popup = document.getElementById("myPopup3");
+      popup.classList.toggle("show")
+  }
+
+  function hobbypop4() {
+    const popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show")
+}
+
+function hobbypop5() {
+    const popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show")
+}
+
